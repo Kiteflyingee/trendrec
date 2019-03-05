2504a94d6e0dfa600c723a8ccb5291835b26b690
liujin <Kiteflyee>
2019-03-05 00:06:31 -0500

    尝试用命令提交

--- UserCF.py
+++ UserCF.py
@@ -7,15 +7,18 @@ Created on 2019年3月2日
 
 
 from utils import *
+import numpy as np
 
 class UserCF:
     '''
     基于用户的协同过滤
     '''
-    def __init__(self, train, test, item_len):
+    def __init__(self, train, test, item_len, topn=10, k=10):
         self.train = train
         self.test = test
         self.item_len = item_len
+        self.topn = topn
+        self.k = k
     
     def cal_degree(self):    
         '''
@@ -23,10 +26,10 @@ class UserCF:
         '''
         self.user_degree = {}
         
-        for user in train:
-            degree = len(train.get(user, set()))
+        for user in self.train:
+            degree = len(self.train.get(user, set()))
             if user in self.user_degree:
-                self.user_degree[user] = self.user_degree[user] + 1
+                self.user_degree[user] = degree
             else:
                 self.user_degree[user] = 0  
                                                 
@@ -46,7 +49,27 @@ class UserCF:
                        continue
                 else:
                     sim = cal_sim(u, v)
-                    user_sim[u][v] = sim
-                    user_sim[v][u] = sim
-                    
-                       
+                    self.user_sim.setdefault(u, {})
+                    self.user_sim.setdefault(v, {})
+                    self.user_sim[u][v] = sim
+                    self.user_sim[v][u] = sim
+
+        return self.user_sim
+
+    
+    def recommend(self, uid):
+        watch_item = self.train.get(uid, set())
+        vector_rank = np.zeros(self.item_len)
+
+        # 先对用户的相似性列表排序
+        sim_list = self.user_sim.get(uid, {})
+        sorted_sim = sorted(sim_list.items(), key=lambda x:x[1], reverse=False)
+        del sim_list
+
+        for v,sim_val in sorted_sim:
+            for item in self.train.get(v, {}):
+                if item in watch_item:
+                    continue
+                vector_rank[item-1] += sim_val
+
+        return np.sort(vector_rank)
