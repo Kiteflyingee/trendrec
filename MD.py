550c5a194fcd49e0297b131943d9e0611b4a5a10
kite <bjliujin@126.com>
2019-05-14 14:50:52 +0800

    submit

--- MD.py
+++ MD.py
@@ -13,8 +13,17 @@ import time
 from sklearn.metrics.pairwise import cosine_similarity
 from PreprocessData import readData
 
-def process_data(train, test, ratio=0.9):
-    df = train.append(test)
+
+
+
+
+
+def process_data(trainset, testset):
+    '''
+    把切分的数据集重新整理数据结构
+    '''
+    
+    df = trainset.append(testset)
     df.rename(colunms={0:'uid',1:'iid'})
     usernum = df.uid.max()+1 #这里多建一列，以防user,item id从1开始
     itemnum = df.iid.max()+1
@@ -23,44 +32,48 @@ def process_data(train, test, ratio=0.9):
     test = {}
 #    行保存user id ，列保存 item id
     train = np.zeros((usernum, itemnum),dtype = np.int32)
-    
-    user_degree = df.uid.value_counts()
-    item_degree = df.iid.value_counts()
-    train_udegree = {}
-    train_idegree = {}
+    # 得到是pandas Series 类型
+    user_degree = trainset.uid.value_counts()
+    item_degree = trainset.iid.value_counts()
     
     traincount = 0
     testcount = 0
-    for idx in train.index: 
-        uid = train.loc[idx,'uid']
-        iid = train.loc[idx,'iid']
-        #        每次更新，相当于对原数据进行减少连边，对应度减少
+    for idx in trainset.index: 
+        uid = trainset.loc[idx,'uid']
+        iid = trainset.loc[idx,'iid']
+        #  填充数据矩阵
         train[uid][iid] = 1
         traincount += 1
 
-    for idx in test.index:
-        uid = test.loc[idx, 'uid']
-        iid = test.loc[idx, 'iid']
+    for idx in testset.index:
+        uid = testset.loc[idx, 'uid']
+        iid = testset.loc[idx, 'iid']
         if uid not in test:
             test[uid] = set()
         test[uid].add(iid)
         testcount += 1
 
-    udegree = pd.Series(train_udegree)
-    idegree = pd.Series(train_idegree)
-#    释放多次引用的变量的内存
-    del train_udegree, train_idegree
-    gc.collect()    
+    #    释放多次引用的变量的内存
     print("训练集%d",traincount)
     print("测试集%d",testcount)
-    return train,test,udegree,idegree
+    return train,test,user_degree,item_degree
+
+
+
+
+
+
+
 
     
 def massDiffisionForOne(train, user, udegree, idegree, K=1000):
-    usernum ,itemnum = np.shape(train)
+    '''
+    对单用户做MassDiffusion过程
+    '''
+    itemnum = np.shape(train)[1]
+    # 这里的usernum和itemnum都是在原始上加1(在id从1开始的数据集，id从0开始的就没有这种情况)
     item_score = np.zeros((itemnum,1),dtype=np.float32)
-    
-    user_score = secondMass(train,idegree, user)
+    user_score = secondMass(train, idegree, user)
     #kNN,选择user端得分最高的K个user进行第三次Mass操作
     user_score = pd.Series(user_score)
     #  这里因为id是从1开始的所以跟index有细微bug，index 0 
@@ -71,37 +84,72 @@ def massDiffisionForOne(train, user, udegree, idegree, K=1000):
         u_itemscore=thirdMass(train, udegree, user_score, uid)
         for item in u_itemscore.keys():
             item_score[item] = u_itemscore[item] + item_score[item]
+    return item_score
+
+
+
+
+
+
+
+
 
 
 def secondMass(train, idegree, uid):
+    '''
+    MassDiffusion第二次传播过程
+    '''
 #    获得该user的item集合
     items = np.nonzero(train[uid, :])[0]
     user_score = np.zeros(train.shape[0],dtype=np.float32)
     for item in items:
-#        找出买过这个item的所有user
+#        找出买过这个item的所有user，id从1开始也不影响
         users=np.nonzero(train[:, item])[0]
 #        每个user打分为,users/degree
         score = 1.0 / idegree[item]
-        
         for user in users:
             user_score[user] = user_score[user] + score
     return user_score
 
-def thirdMass(train, udegree, user_score, uid):    
+
+
+
+
+
+
+
+
+def thirdMass(train, udegree, user_score, uid):   
+    '''
+    massdiffusion 第三次传播过程
+    ''' 
     items = np.nonzero(train[uid, :])[0]
     u_itemscore = {}
     score = user_score[uid]/udegree[uid]
     for item in items:
         u_itemscore[item] = score
     return u_itemscore
-    
+
+
+
+
+
+
+
+
+
+
 def exp():
     '''
     编写实验逻辑
     '''
     filepath = r'./data/netflix5k_result.txt'
     train_data, test_data = readData(filepath, split=',', train_ratio=0.7)    
-    process_data(train_data,test_data,0.9)
+    train,test,udegree,idegree = process_data(train_data,test_data,0.9)
+    # for user in train.keys()
 
 if __name__ == '__main__':
+    '''
+    入口
+    '''
     exp()
